4c6cc2679dd219d564c1f6b34934f4c386f6051a
elsie <elsiechen915@gmail.com>
2023-06-29 00:22:17 -0400

    Write reverseString() and tests

--- index.js
+++ index.js
@@ -2,4 +2,20 @@ function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export { capitalize };
+function reverseString(string) {
+  // JavaScript string is immutable, but array is mutable
+  // Use spllit('') to convert string to an array
+  let array = string.split(''); 
+  let length = array.length;
+  let loopLength = length % 2 === 0? length / 2: (length-1) / 2;
+  
+  for(let i = 0; i < loopLength; i++) {
+     let temp = array[i];
+     array[i] = array[length - 1 - i];
+     array[length - 1 - i] = temp;
+  }
+  // Use join('') to convert the array back to a string
+  return array.join(''); 
+}
+
+export { capitalize,reverseString };
--- index.test.js
+++ index.test.js
@@ -1,6 +1,14 @@
-import { capitalize } from "./index";
+import { capitalize, reverseString } from "./index";
 
 test('capitalize strings', () => {
     expect(capitalize('morning')).toMatch(/Morning/);
     expect(capitalize('TEST')).toMatch(/TEST/);
+});
+
+test('reverse odd number characters of string', () => {
+    expect(reverseString('eat')).toMatch(/tae/);
+});
+
+test('reverse even number characters of string', () => {
+    expect(reverseString('jacket')).toMatch(/tekcaj/);
 });
