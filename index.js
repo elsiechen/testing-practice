5b87b2ca1bc1255e8327ec0950d1b30ca4371f83
elsie <elsiechen915@gmail.com>
2023-06-29 22:46:42 -0400

    Write caesarCipher() and tests

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function capitalize(string) {
 
 function reverseString(string) {
   // JavaScript string is immutable, but array is mutable
-  // Use spllit('') to convert string to an array
+  // Use split('') to convert string to an array
+
+
   let array = string.split(''); 
   let length = array.length;
   let loopLength = length % 2 === 0? length / 2: (length-1) / 2;
@@ -18,4 +20,35 @@ function reverseString(string) {
   return array.join(''); 
 }
 
-export { capitalize,reverseString };
+function Calculator(a, b) {
+  this.add = () => a + b;
+  this.subtract = () => a - b;
+  this.divide = () => {
+    if(b === 0) throw Error('Second number can not be zero');
+    return a / b;
+  };
+  this.multiply = () => a * b;
+}
+
+function caesarCipher(string, key) {
+  // if key is not a positive number, throw error
+  if(key < 0 || !Number.isInteger(key)) throw Error('Key should be a positive number.');
+   
+  let array = [];
+  for(let i = 0; i < string.length; i++) {
+     // symbol or punctuation, leave it untouched
+    if(string[i].toLowerCase() === string[i].toUpperCase()) {
+      array.push(string[i]);
+    } else { // characters, get shifted 
+      let unitCode = string.charCodeAt(i); 
+      // UpperCase is 65 to 90; LowerCase is 97 to 122
+      let startValue = (unitCode >= 65 && unitCode <= 90)? 65: 97;
+      let shiftedCode = (unitCode - startValue + key) % 26;
+      let shiftedCharacter = String.fromCharCode(parseInt(shiftedCode) + startValue)
+      array.push(shiftedCharacter);
+    }
+  }
+  // use join() to convert array into string
+  return array.join('');
+}
+export { capitalize,reverseString, Calculator, caesarCipher };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./index";
+import { capitalize, reverseString, Calculator, caesarCipher } from "./index";
 
 test('capitalize strings', () => {
     expect(capitalize('morning')).toMatch(/Morning/);
@@ -11,4 +11,61 @@ test('reverse odd number characters of string', () => {
 
 test('reverse even number characters of string', () => {
     expect(reverseString('jacket')).toMatch(/tekcaj/);
+});
+
+test('basic math operations with 3 and 9', () => {
+    let cal = new Calculator(3, 9);
+    expect(cal.add()).toBe(12);
+    expect(cal.subtract()).toBe(-6);
+    expect(cal.divide()).toBeCloseTo(0.33);
+    expect(cal.multiply()).toBe(27);
+});
+
+test('basic math operations with 4 and 0', () => {
+    let cal = new Calculator(4, 0);
+    expect(cal.add()).toBe(4);
+    expect(cal.subtract()).toBe(4);
+    expect(() => cal.divide()).toThrow(Error);
+    expect(() => cal.divide()).toThrow('Second number can not be zero');
+    expect(cal.multiply()).toBe(0);
+});
+
+test('basic math operations with 3.5 and 9.7', () => {
+    let cal = new Calculator(3.5, 9.7);
+    expect(cal.add()).toBeCloseTo(13.2);
+    expect(cal.subtract()).toBeCloseTo(-6.2);
+    expect(cal.divide()).toBeCloseTo(0.36);
+    expect(cal.multiply()).toBeCloseTo(33.95);
+});
+
+test('basic math operations with -3.5 and 9.7', () => {
+    let cal = new Calculator(-3.5, 9.7);
+    expect(cal.add()).toBeCloseTo(6.2);
+    expect(cal.subtract()).toBeCloseTo(-13.2);
+    expect(cal.divide()).toBeCloseTo(-0.36);
+    expect(cal.multiply()).toBeCloseTo(-33.95);
+});
+
+test('encrypt lower case characters with different positive keys', () => {
+    expect(caesarCipher('abcde', 1)).toMatch('bcdef');
+    expect(caesarCipher('abcde', 0)).toMatch('abcde');
+    expect(caesarCipher('abcde', 260)).toMatch('abcde');
+});
+
+test('encrypt lower and upper characters with case unchanged', () => {
+    expect(caesarCipher('ABCDE', 1)).toMatch('BCDEF');
+    expect(caesarCipher('AbCdE', 0)).toMatch('AbCdE');
+    expect(caesarCipher('abcDEF', 2)).toMatch('cdeFG');
+});
+
+test('encryption keeps symbols and punctuations unchanged', () => {
+    expect(caesarCipher('Aa!#?', 1)).toMatch('Bb!#?');
+    expect(caesarCipher('Zz + - / *', 2)).toMatch('Bb + - / *');
+    expect(caesarCipher('abc123', 2)).toMatch('cde123');
+});
+
+test('encryption with negative or non-number keys throws errors', () => {
+    expect(() => {caesarCipher('abcdefg', -2)}).toThrow(Error);
+    expect(() => {caesarCipher('abcdefg', 'a')}).toThrow('Key should be a positive number.');
+    expect(() => {caesarCipher('abcdefg', '!')}).toThrow(Error);
 });
