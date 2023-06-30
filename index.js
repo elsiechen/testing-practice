7d8d724027f3e46eee301f5213d1412123f11844
elsie <elsiechen915@gmail.com>
2023-06-30 00:11:14 -0400

    Write analyzeArray() and tests

--- index.js
+++ index.js
@@ -51,4 +51,35 @@ function caesarCipher(string, key) {
   // use join() to convert array into string
   return array.join('');
 }
-export { capitalize,reverseString, Calculator, caesarCipher };
+
+function analyzeArray(array) {
+  if(array.length === 0) throw Error('Please enter elements in the array.');
+  if(!Array.isArray(array)) throw Error('Please enter an array.');
+  
+  const average = () => {
+    return array.reduce((prev, curr) => prev + curr, 0) / array.length;
+  };
+  const min = () => {
+    let value = array[0];
+    for(let i = 0; i < array.length; i++) {
+      if(typeof array[i] !== 'number') throw Error('Non-number detected! Please enter all numbers in the array.')
+      if(value > array[i]) value = array[i];
+    }
+    return value;
+  };
+  const max = () => {
+    let value = array[0];
+    for(let i = 0; i < array.length; i++) {
+      if(typeof array[i] !== 'number') throw Error('Non-number detected! Please enter all numbers in the array.')
+      if(value < array[i]) value = array[i];
+    }
+    return value;
+  };
+  const length = () => array.length;
+  return {  average: average(array),
+            min: min(array),
+            max: max(array),
+            length: length(array) 
+         }
+  }
+export { capitalize,reverseString, Calculator, caesarCipher, analyzeArray };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString, Calculator, caesarCipher } from "./index";
+import { capitalize, reverseString, Calculator, caesarCipher, analyzeArray } from "./index";
 
 test('capitalize strings', () => {
     expect(capitalize('morning')).toMatch(/Morning/);
@@ -69,4 +69,25 @@ test('encryption with negative or non-number keys throws errors', () => {
     expect(() => {caesarCipher('abcdefg', 3.5)}).toThrow('Key should be a positive integer.');
     expect(() => {caesarCipher('abcdefg', 'a')}).toThrow('Key should be a positive integer.');
     expect(() => {caesarCipher('abcdefg', '!')}).toThrow(Error);
+});
+
+test('analyze array with numbers', () => {
+    expect(analyzeArray([1,8,3,4,2,6])).toMatchObject({
+        average: 4, min: 1, max: 8, length: 6
+    });
+    expect(analyzeArray([2])).toMatchObject({
+        average: 2, min: 2, max: 2, length: 1
+    });
+});
+
+test('analyze empty array will throw error', () => {
+    expect(() => {analyzeArray([])}).toThrow('Please enter elements in the array.')
+});
+
+test('analyze non array will throw error', () => {
+    expect(() => {analyzeArray('abc')}).toThrow('Please enter an array.')
+});
+
+test('analyze array with non-number will throw error', () => {
+    expect(() => {analyzeArray([2,'a',5,'?'])}).toThrow('Non-number detected! Please enter all numbers in the array.')
 });
